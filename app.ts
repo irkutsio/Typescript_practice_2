688eee2761996c0abe6d8807d2ae16f5b3b85608
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-11-23 00:18:15 +0200

    compiler

--- app.ts
+++ app.ts
@@ -23,19 +23,74 @@ const person: {
 	user: [34, 'user'],
 };
 
-person.user.push(34) // push це виключення
+person.user.push(34); // push це виключення
 
-console.log(person.user) //works 
+console.log(person.user); //works
 
 //union === |
 
-function add (num1 : number | string, num2 : number | string) {
-    if (typeof num1 === 'number' && typeof num2 === 'number')
-    return num1 + num2
-}
+// function add (num1 : number | string, num2 : number | string) {
+//     if (typeof num1 === 'number' && typeof num2 === 'number')
+//     return num1 + num2
+// }
 
-console.log(add(6,900))
+// console.log(add(6,900))
 
 const a = 'a';
-const b = 'b'
-console.log(a + b)
+const b = 'b';
+console.log(a + b);
+
+// function add(num1: number, num2: number): string {
+// 	let result = 0;
+// 	result = num1 + num2;
+// 	return result.toString(); //works
+// }
+
+// console.log(add(67,56))
+
+// function add(num1: number, num2: number) : void{
+// 	console.log(num1 + num2);
+// }
+
+// add(67, 56);
+
+// function add(num1: number, num2: number) : number{
+// 	return num1 + num2;
+// }
+// add(67, 56);
+
+////////////////////параметр\\\параметр\\\\\\return
+// let functionType : (a: number, b: number) => number;
+// functionType = add;
+// console.log(functionType(3,4))
+
+//******************************************************************************** */
+function add(num1: number, num2: number, cb: (num: number) => void) {
+	const result = num1 + num2;
+	cb(result);
+}
+
+add(10, 20, result => {
+	console.log(result);
+});
+//********************************************************************************* */
+
+function sendRequest(data: string, cb: (response: any) => void) {
+	// ... sending a request with "data"
+	return cb({ data: 'Hi there!' });
+}
+
+sendRequest('Send this!', response => {
+	console.log(response);
+	return true;
+});
+
+//******************************
+//never
+
+function generateError(message: string, code: number): never {
+	//because 'throw' does NOT return undefined, return NOTHUNG => never
+	throw { message, code };
+}
+
+generateError('oops, something goes wrong', 404)
